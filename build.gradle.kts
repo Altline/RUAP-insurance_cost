04159b43de4ea58afee18bd233a49f8324b37dc0
Dominik Živko <domy.zivko@gmail.com>
2022-01-27 23:27:45 +0100

    Set up dependencies and logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,19 @@
+val kotlinVersion: String by System.getProperties()
+val ktorVersion = "1.6.7"
+val kotlinHtmlVersion = "0.7.3"
+val kotlinCoroutinesVersion = "1.6.0"
+val serializationVersion = "1.3.2"
+val retrofitVersion = "2.9.0"
+val okHttpLoggingInterceptorVersion = "4.9.3"
+val log4jVersion = "2.17.1"
+
+val kotlinReactVersion = "17.0.2-pre.290-kotlin-$kotlinVersion"
+val kotlinStyledVersion = "5.3.3-pre.290-kotlin-$kotlinVersion"
+
 plugins {
-    kotlin("multiplatform") version "1.6.10"
+    val kotlinVersion: String by System.getProperties()
+    kotlin("multiplatform") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
     application
 }
 
@@ -7,7 +21,6 @@ group = "altline"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    jcenter()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
 }
@@ -28,21 +41,37 @@ kotlin {
         }
     }
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+            }
+        }
         val commonTest by getting
         val jvmMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-server-netty:1.6.7")
-                implementation("io.ktor:ktor-html-builder:1.6.7")
-                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.3")
+                implementation("io.ktor:ktor-server-core:$ktorVersion")
+                implementation("io.ktor:ktor-server-netty:$ktorVersion")
+                implementation("io.ktor:ktor-serialization:$ktorVersion")
+                implementation("io.ktor:ktor-html-builder:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinHtmlVersion")
+                implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+                implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+                implementation("com.squareup.okhttp3:logging-interceptor:$okHttpLoggingInterceptorVersion")
+                implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
+                implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
+                implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
             }
         }
         val jvmTest by getting
         val jsMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.290-kotlin-1.6.10")
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.290-kotlin-1.6.10")
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.3-pre.290-kotlin-1.6.10")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react:$kotlinReactVersion")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:$kotlinReactVersion")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:$kotlinStyledVersion")
+                implementation("io.ktor:ktor-client-js:$ktorVersion")
+                implementation("io.ktor:ktor-client-json:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
             }
         }
         val jsTest by getting
@@ -50,7 +79,7 @@ kotlin {
 }
 
 application {
-    mainClass.set("altline.application.ServerKt")
+    mainClass.set("altline.insurance.ServerKt")
 }
 
 tasks.named<Copy>("jvmProcessResources") {
