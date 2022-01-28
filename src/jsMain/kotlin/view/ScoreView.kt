d3f83fa1d0e5ed4fae191d1abd32347eb017c19c
Dominik Živko <domy.zivko@gmail.com>
2022-01-28 23:11:18 +0100

    Add score view

---
+++ src/jsMain/kotlin/view/ScoreView.kt
@@ -0,0 +1,28 @@
+package view
+
+import Features
+import react.Props
+import react.RBuilder
+import react.dom.div
+import react.fc
+
+external interface ScoreViewProps : Props {
+    var scoredFeatures: Features?
+}
+
+val scoreView = fc<ScoreViewProps> { props ->
+    div {
+        val scoreMean = props.scoredFeatures?.scoredLabelMean?.toString() ?: "-"
+        +"Score mean: $scoreMean"
+    }
+    div {
+        val scoreDeviation = props.scoredFeatures?.scoredLabelStandardDeviation?.toString() ?: "-"
+        +"Score standard deviation $scoreDeviation"
+    }
+}
+
+fun RBuilder.scoreView(scoredFeatures: Features? = null) {
+    child(scoreView) {
+        attrs.scoredFeatures = scoredFeatures
+    }
+}
