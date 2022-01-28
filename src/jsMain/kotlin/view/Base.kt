61caaba4aea9e6b79a90deafde9d86779786e403
Dominik Živko <domy.zivko@gmail.com>
2022-01-28 20:55:40 +0100

    Add feature input component

--- src/jsMain/kotlin/view/Base.kt
+++ src/jsMain/kotlin/view/Base.kt
@@ -1,11 +1,14 @@
 package view
 
+import Features
 import PredictionService
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import react.Props
 import react.RBuilder
 import react.RComponent
 import react.State
+import react.dom.div
 
 external interface BaseProps : Props {
     var coroutineScope: CoroutineScope
@@ -17,6 +20,19 @@ external interface BaseState : State
 class Base : RComponent<BaseProps, BaseState>() {
 
     override fun RBuilder.render() {
+        div {
+            child(featureInput) {
+                attrs.onSubmit = { features ->
+                    props.coroutineScope.launch {
+                        val prediction = submitForPrediction(features)
+                        println(prediction)
+                    }
+                }
+            }
+        }
+    }
 
+    private suspend fun submitForPrediction(features: Features): Features {
+        return props.predictionService.getPrediction(features)
     }
 }
---
+++ src/jsMain/kotlin/view/FeatureInput.kt
@@ -0,0 +1,97 @@
+package view
+
+import Features
+import kotlinx.html.InputType
+import kotlinx.html.id
+import kotlinx.html.js.onChangeFunction
+import org.w3c.dom.HTMLInputElement
+import react.Props
+import react.RBuilder
+import react.dom.*
+import react.fc
+import react.useState
+
+external interface FeatureInputProps : Props {
+    var onSubmit: (Features) -> Unit
+}
+
+val featureInput = fc<FeatureInputProps> { props ->
+    val features by useState(Features())
+
+    form {
+        attrs.onSubmit = {
+            it.preventDefault()
+            props.onSubmit(features)
+        }
+
+        intInput("age", "Age") {
+            features.age = it
+        }
+        intInput("height", "Height") {
+            features.height = it
+        }
+        intInput("weight", "Weight") {
+            features.weight = it
+        }
+        booleanInput("allergies", "Known allergies") {
+            features.knownAllergies = it
+        }
+        booleanInput("diabetes", "Diabetes") {
+            features.diabetes = it
+        }
+        booleanInput("bpp", "Blood pressure problems") {
+            features.bloodPressureProblems = it
+        }
+        booleanInput("chronic", "Any chronic diseases") {
+            features.anyChronicDiseases = it
+        }
+        booleanInput("cancer", "History of cancer in family") {
+            features.historyOfCancerInFamily = it
+        }
+        booleanInput("transplants", "Any transplants") {
+            features.anyTransplants = it
+        }
+        intInput( "surgeries", "Number of major surgeries") {
+            features.numberOfMajorSurgeries = it
+        }
+
+        input(InputType.submit) {
+            attrs.value = "Submit"
+        }
+    }
+}
+
+private fun RBuilder.intInput(id: String, labelText: String, onChangeValue: (Int) -> Unit) {
+    div {
+        label {
+            +labelText
+            attrs.htmlFor = id
+        }
+        input(InputType.number) {
+            attrs.id = id
+            attrs.onChangeFunction = { event ->
+                event.runCatching {
+                    (event.target as HTMLInputElement).value.toInt()
+                }.onSuccess {
+                    onChangeValue(it)
+                }
+            }
+        }
+    }
+}
+
+private fun RBuilder.booleanInput(id: String, labelText: String, onChangeValue: (Boolean) -> Unit) {
+    div {
+        label {
+            +labelText
+            attrs.htmlFor = id
+        }
+        input(InputType.checkBox) {
+            attrs.id = id
+            attrs.onChangeFunction = { event ->
+                val newValue = (event.target as HTMLInputElement).checked
+                onChangeValue(newValue)
+            }
+        }
+    }
+}
